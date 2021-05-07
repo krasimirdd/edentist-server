5581a35c105f820e8a486899aac81a83c880b322
Krasimir Dimitrov <kdduni98@gmail.com>
2021-05-07 20:08:17 +0300

    imporst

--- src/main/java/com/kdimitrov/edentist/common/api/graphql/Query.java
+++ src/main/java/com/kdimitrov/edentist/common/api/graphql/Query.java
@@ -1,11 +1,8 @@
 package com.kdimitrov.edentist.common.api.graphql;
 
-import com.kdimitrov.edentist.common.models.dto.DoctorDto;
 import com.kdimitrov.edentist.common.services.DefService;
 import graphql.kickstart.tools.GraphQLQueryResolver;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class Query implements GraphQLQueryResolver {
